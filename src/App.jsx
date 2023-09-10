5d80999c87dc1f354ac7e3c9f220239439bc28f0
Cristhian Vera <cristianmario092190@gmail.com>
2023-09-10 12:46:34 -0500

    Todos List completed  in a traditional way

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useRef, useState } from "react";
+import "./styles.css";
 function App() {
-  const [count, setCount] = useState(0)
+  //para el focus del input text
+  const inputRef = useRef(null);
+  //estado para los items del input text a agregar
+  const [newItem, setNewItem] = useState("");
+  //estado para los Todo List
+  const [todos, setTodos] = useState([]);
+  function handleChangeItem(event) {
+    setNewItem(event.target.value);
+  }
+  function handleAddTodo(event) {
+    event.preventDefault();
+    setTodos([
+      ...todos,
+      {
+        id: crypto.randomUUID(),
+        title: newItem,
+        completed: false,
+      },
+    ]);
+    setNewItem("");
+    inputRef.current.focus();
+  }
+
+  function toggleTodo(id, checked) {
+    // le currenTodos tendra actualmente todo el array de toods
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) return { ...todo, completed: checked };
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    //(currentTodos) =>{} ---> function (currentTodos){}
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+  //console.log(todos);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form className="new-item-form" onSubmit={handleAddTodo}>
+        <div className="form-row">
+          <label htmlFor="txt_task">Name Task</label>
+          <input
+            type="text"
+            name=""
+            id="txt_task"
+            value={newItem}
+            onChange={handleChangeItem}
+            ref={inputRef}
+            required
+          />
+        </div>
+
+        <button className="btn">Add Task</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        {todos.length === 0 && "No Todos"}
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  name=""
+                  id=""
+                  onChange={(event) =>
+                    toggleTodo(todo.id, event.target.checked)
+                  }
+                />
+                {todo.title}
+              </label>
+              <button
+                className="btn btn-danger"
+                onClick={() => deleteTodo(todo.id)}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
